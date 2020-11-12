566e303c0ebc7b89ca076b58893824da880b87fe
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2020-11-12 12:08:47 -0600

    Deployed the app

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-5cc42/us-central1/api' 
+    baseURL: 'https://us-central1-challenge-5cc42.cloudfunctions.net/api'
+    //'http://localhost:5001/challenge-5cc42/us-central1/api' 
     //THE API (cloud function) URL
 });
 
 export default instance;
+
+
